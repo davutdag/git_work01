37a9bbbfd4a7a62ad7b2987ca422cf0891e68346
davutdag <davutdag35@gmail.com>
2020-10-08 22:25:58 +0300

    feature ilk commit

--- src/git_work01/MyClass.java
+++ src/git_work01/MyClass.java
@@ -3,8 +3,9 @@ package git_work01;
 public class MyClass {
 
 	public static void main(String[] args) {
-	System.out.println("Eclipse den gite wor_01");
+	System.out.println("Eclipse den gite work_01");
 	System.out.println("GitHub dan Eclipse");
+	System.out.println("feature da ilk commit");
 	}
 
 }
